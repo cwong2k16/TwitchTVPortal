503f470144079c70d3bce005732039f1ed3d817f
cwong2015 <sirhc8002@gmail.com>
2017-07-02 11:08:18 -0400

    trying to figure out the workarounds to the twitch api

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-  
+    $.getJSON('https://crossorigin.me/https://api.twitch.tv/kraken/streams/freecodecamp', function(data) {
+      $("#test").html(data);
+    });
 });
