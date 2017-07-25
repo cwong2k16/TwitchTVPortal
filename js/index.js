f89516bd853d335d0082a835277b35c1de67b70e
cwong2015 <sirhc8002@gmail.com>
2017-07-25 14:43:57 -0400

    basic button functionality started for all button

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-//var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-var users = ["ESL_SC2", "freecodecamp", "1mg85jooqkr"];
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "comster404", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+var currentDisplay = "all";
 
 $(document).ready(function(){
     for(var i = 0; i < users.length; i++){
@@ -8,18 +9,27 @@ $(document).ready(function(){
             console.log(data);
             if(data.status === 404){
                 $("#test").append("<p> " + users[i] + ":<span class = 'red'>" + " DOESN'T EXIST!!</span>" + "</p>");
+                users[i].status = "invalid";
             }
             else{
                 $.getJSON('https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/streams/' + users[i], function(data) {
                 if(data.stream === null){
                     $("#test").append("<p> " + users[i] + ":<a target = '_blank' href =" + "https://www.twitch.tv/" + users[i] + "> " + "<span class = 'red'>" + "OFFLINE</span>" + "</p>");
+                    users[i].status = "offline"
                 }
                 else{
                     $("#test").append("<p> " + users[i] + ":<a target = '_blank' href =" + "https://www.twitch.tv/" + users[i] + "> " + "<span class = 'green'>" + "ONLINE</span>" + "</a>" + " --Currently streaming " + data.stream.game + "</p>");
+                    users[i].status = "online";
                 }
               });
             }
         });   
       })(i)
     };
+    $("#all").click(function(){
+        if(currentDisplay !== "all"){
+            currentDisplay == "all";
+            $("#test").empty();
+        }
+    });
 });
