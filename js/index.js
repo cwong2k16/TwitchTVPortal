b8298d627cffe5026183589f2b26ff024e1c92a6
cwong2015 <sirhc8002@gmail.com>
2017-07-24 23:59:50 -0400

    great. good chunk of it done, pretty much just need to style it better.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
 $(document).ready(function(){
-    $.getJSON('https://crossorigin.me/https://api.twitch.tv/kraken/streams/freecodecamp', function(data) {
-      $("#test").html(data);
-    });
+    for(var i = 0; i < users.length; i++){
+        (function(i){
+        $.getJSON('https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/streams/' + users[i], function(data) {
+            console.log(data);
+            if(data.stream === null){
+                $("#test").append("<p> " + users[i] + "<a target = '_blank' href =" + "https://www.twitch.tv/" + users[i] + "> " + "<span class = 'red'>" + "OFFLINE</span>" + "</p>");
+            }
+            else{
+                $("#test").append("<p> " + users[i] + "<a target = '_blank' href =" + "https://www.twitch.tv/" + users[i] + "> " + "<span class = 'green'>" + "ONLINE</span>" + "</a>" + " --Currently streaming " + data.stream.game + "</p>");
+            }
+        });   
+      })(i)
+    };
 });
