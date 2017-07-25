01123dc4fee2a94d5fc66f0f378fc0b6c63ba13c
cwong2015 <sirhc8002@gmail.com>
2017-07-25 11:46:23 -0400

    updated the styling a bit with the scss file

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
-var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+//var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var users = ["ESL_SC2", "freecodecamp", "1mg85jooqkr"];
 
 $(document).ready(function(){
     for(var i = 0; i < users.length; i++){
         (function(i){
-        $.getJSON('https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/streams/' + users[i], function(data) {
+        $.getJSON('https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/channels/' + users[i], function(data) {
             console.log(data);
-            if(data.stream === null){
-                $("#test").append("<p> " + users[i] + "<a target = '_blank' href =" + "https://www.twitch.tv/" + users[i] + "> " + "<span class = 'red'>" + "OFFLINE</span>" + "</p>");
+            if(data.status === 404){
+                $("#test").append("<p> " + users[i] + ":<span class = 'red'>" + " DOESN'T EXIST!!</span>" + "</p>");
             }
             else{
-                $("#test").append("<p> " + users[i] + "<a target = '_blank' href =" + "https://www.twitch.tv/" + users[i] + "> " + "<span class = 'green'>" + "ONLINE</span>" + "</a>" + " --Currently streaming " + data.stream.game + "</p>");
+                $.getJSON('https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/streams/' + users[i], function(data) {
+                if(data.stream === null){
+                    $("#test").append("<p> " + users[i] + ":<a target = '_blank' href =" + "https://www.twitch.tv/" + users[i] + "> " + "<span class = 'red'>" + "OFFLINE</span>" + "</p>");
+                }
+                else{
+                    $("#test").append("<p> " + users[i] + ":<a target = '_blank' href =" + "https://www.twitch.tv/" + users[i] + "> " + "<span class = 'green'>" + "ONLINE</span>" + "</a>" + " --Currently streaming " + data.stream.game + "</p>");
+                }
+              });
             }
         });   
       })(i)
